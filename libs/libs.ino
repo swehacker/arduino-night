4e91cccfdfb8bc0037070aac29594f128c454afb
Sebastian Johansson <johansson.sebastian@gmail.com>
2016-04-30 21:10:02 +0200

    Added libs tutorial with lux sensor

---
+++ libs/libs.ino
@@ -0,0 +1,43 @@
+#include <Wire.h>
+#include <SparkFunTSL2561.h>
+#include <Adafruit_Sensor.h>
+
+SFE_TSL2561 light;
+boolean gain;     // Gain setting, 0 = X1, 1 = X16;
+unsigned int ms;  // Integration ("shutter") time in milliseconds
+
+void setup() {
+
+    Serial.begin(9600);
+    light.begin();
+
+    gain = 0;
+    unsigned char time = 2;
+
+    Serial.println("Set timing...");
+    light.setTiming(gain,time,ms);
+
+  // To start taking measurements, power up the sensor:
+  
+  Serial.println("Powerup...");
+  light.setPowerUp();
+
+}
+
+void loop() {
+  
+   delay(ms);
+   unsigned int data0, data1;
+  
+  if (light.getData(data0,data1))
+  {
+    double lux;    // Resulting lux value
+    boolean good;  // True if neither sensor is saturated
+
+    good = light.getLux(gain,ms,data0,data1,lux);
+  
+    Serial.print(" lux: ");
+    Serial.print(lux);
+    if (good) Serial.println(" (good)"); else Serial.println(" (BAD)");
+  }
+}
